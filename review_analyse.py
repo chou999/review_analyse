778153b05cf3ce6463d9be171866914ac1454c29
CCChou <83779490+chou999@users.noreply.github.com>
2022-01-09 03:25:00 +0800

    calc avg

--- review_analyse.py
+++ review_analyse.py
@@ -5,8 +5,11 @@ with open('reviews.txt', 'r') as f:
 		data.append(line.strip())
 		count += 1 # count = count + 1
 		if count % 1000 == 0:
-		    print(count)
-print(len(data))
-print(data[0])
-print('---------------')
-print(data[1])
+			print(count)
+print('檔案讀取完了，總共有', len(data), '筆資料')
+
+sum_len = 0
+for d in data:
+	sum_len += len(d) # sum_len = sum_len + len(d)
+
+print('留言的平均長度為', sum_len/len(data))

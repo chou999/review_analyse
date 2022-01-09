215b872aed4cc69d9ea80c6b049ac8816a482025
CCChou <83779490+chou999@users.noreply.github.com>
2022-01-09 18:24:55 +0800

    add filter function

--- review_analyse.py
+++ review_analyse.py
@@ -13,3 +13,11 @@ for d in data:
 	sum_len += len(d) # sum_len = sum_len + len(d)
 
 print('留言的平均長度為', sum_len/len(data))
+
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '資料長度小於100')
+print('new清單的第一筆資料為\n', new[0])
+print('new清單的第十筆資料為\n',new[11])
